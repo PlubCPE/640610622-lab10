54516cb6624e3c0bcc5d8a067126f47977dc4aa8
PlubCPE <chakkapop_s@cmu.ac.th>
2021-12-14 18:15:01 +0700

    lab10_1_complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,11 +2,14 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double prevbal, percent, amount, interest, total, newba; 	
 	cout << "Enter initial loan: ";
+	cin  >> prevbal;
 	cout << "Enter interest rate per year (%): ";
+	cin  >> percent;
 	cout << "Enter amount you can pay per year: ";
-
+	cin  >> amount;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -20,14 +23,28 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+		for(int i = 1 ; ; i++)
+		{ 	
+			if(prevbal > 0)
+			{
+						cout << fixed << setprecision(2);
+						cout << setw(13) << left << i; 
+						cout << setw(13) << left << prevbal;
+						cout << setw(13) << left << (interest = prevbal*(percent/100));
+						cout << setw(13) << left << (total = prevbal + interest);
+						if(amount > total)
+						{
+							amount = total;
+						}
+						cout << setw(13) << left << amount;
+						cout << setw(13) << left << (newba = total - amount);
+						cout << "\n";
+						prevbal = newba;
+			}
+			
+		}
+	
+
 	return 0;
 }
